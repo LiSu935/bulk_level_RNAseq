3c6bcdaa6059a9076d9a66b6ffe4b8af94bef403
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2022-06-15 16:16:20 -0500

    Update genie3_results_parsing.py

--- genie3_results_parsing.py
+++ genie3_results_parsing.py
@@ -18,14 +18,20 @@ def parsing_results(input_dir, fi_name):
                     dic_tf[line_list[0]].append((line_list[1], float(line_list[2])))
 
     print(len(dic_tf))
-
+    
+    sorted_dic = sorted(dic_tf.items(), key=lambda x:len(x))
+    for i, j in enumerate(sorted_dic):
+        if i <10:
+            print(j[0]+'\t'+'\t'.join(j[1]))
+        
+    print("======== links end =================")
     len_list = []
     for tf in dic_tf.keys():
         len_list.append(len(dic_tf[tf]))
 
     print(sorted(len_list)[0])
     print(sorted(len_list)[-1])
-    print(len_list)
+    print(sorted(len_list))
     
 for fi in os.listdir(input_dir):
   if fi.endswith(".txt"):
