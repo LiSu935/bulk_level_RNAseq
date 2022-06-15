c4faaa4dcf2ddc6a7f3bc30a73ef2fa931956159
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2022-06-15 15:05:05 -0500

    Update genie3_results_parsing.py

--- genie3_results_parsing.py
+++ genie3_results_parsing.py
@@ -1,25 +1,34 @@
-fi_name = 'Cortex_72h_Bj_edited_GENIE3linkList_0.005.txt'
+import os 
+
+#fi_name = 'Cortex_72h_Bj_edited_GENIE3linkList_0.005.txt'
 input_dir = '/storage/htc/joshilab/Su_Li/GaryLab/Jaehyo_RNAseq/genie3_jobs/results/'
 
-
-dic_tf = {}
-with open(input_dir+fi_name, 'r') as f:
-    for line in f:
-        if line.startswith('""'):
-            continue
-        else:
-            line_list = line.rstrip().split('\t')[1:]
-            if line_list[0] not in dic_tf.keys():
-                dic_tf[line_list[0]] = [(line_list[1], float(line_list[2]))]
+def parsing_results(input_dir, fi_name):
+    
+    dic_tf = {}
+    with open(input_dir+fi_name, 'r') as f:
+        for line in f:
+            if line.startswith('""'):
+                continue
             else:
-                dic_tf[line_list[0]].append((line_list[1], float(line_list[2])))
+                line_list = line.rstrip().split('\t')[1:]
+                if line_list[0] not in dic_tf.keys():
+                    dic_tf[line_list[0]] = [(line_list[1], float(line_list[2]))]
+                else:
+                    dic_tf[line_list[0]].append((line_list[1], float(line_list[2])))
 
-print(len(dic_tf))
+    print(len(dic_tf))
 
-len_list = []
-for tf in dic_tf.keys():
-    len_list.append(len(dic_tf[tf]))
+    len_list = []
+    for tf in dic_tf.keys():
+        len_list.append(len(dic_tf[tf]))
 
-print(sorted(len_list)[0])
-print(sorted(len_list)[-1])
-print(len_list)
+    print(sorted(len_list)[0])
+    print(sorted(len_list)[-1])
+    print(len_list)
+    
+for fi in os.listdir(input_dir):
+  if fi.endswith(".txt"):
+    print(fi)
+    parsing_results(input_dir, fi)
+    print("=========================")
